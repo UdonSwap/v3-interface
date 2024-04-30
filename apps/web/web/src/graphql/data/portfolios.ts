f4acaaf9b2d2e4dea67aaab07828a112f2551ce9
Lajja123 <lajja.lamprostech@gmail.com>
2024-04-30 15:48:56 +0530

    latest code with error resolved of price token

--- apps/web/web/src/graphql/data/portfolios.ts
+++
@@ -1,3 +0,0 @@
-import { PortfolioTokenBalancePartsFragment } from 'uniswap/src/data/graphql/uniswap-data-api/__generated__/types-and-hooks'
-
-export type PortfolioToken = NonNullable<PortfolioTokenBalancePartsFragment['token']>
